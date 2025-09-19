0d337b737f65e58922d4ec9d9a4f96a7c288e046
Ashley Hindle <hey@ashleyhindle.com>
2025-09-19 09:23:32 +0100

    fix eslint issues

--- resources/js/components/delete-user.tsx
+++ resources/js/components/delete-user.tsx
@@ -1,10 +1,7 @@
 import ProfileController from '@/actions/App/Http/Controllers/Settings/ProfileController';
 import HeadingSmall from '@/components/heading-small';
-import InputError from '@/components/input-error';
 import { Button } from '@/components/ui/button';
 import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 import { Form } from '@inertiajs/react';
 import { useRef } from 'react';
 
